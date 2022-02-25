2ddd729b648f0934cdbf2b7ce3e7fd78499c22c9
Mitchell Reyes <mr32dev@gmail.com>
2022-02-25 22:01:10 +0000

    Testing

--- yt-content/src/App.js
+++ yt-content/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React idk
+          Learn React last for now
         </a>
       </header>
     </div>
